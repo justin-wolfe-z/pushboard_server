b8de0d6626fa0208d1264620a34fb8b004774efc
justin-wolfe-z <justin.wolfe@zapier.com>
2017-11-23 12:55:51 -0500

    using mongoose to connect to local db without error, made tweaks to avoid deprecation issues

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ express = require('express')
 app = express()
 port = process.env.PORT || 4000
 mongoose = require('mongoose')
+mongoose.Promise = Promise;
 User = require('./model')
 bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-/*mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/ooodb');*/
+/*mongoose.connect('mongodb://localhost/')*/
+
+const db_promise = mongoose.connect('mongodb://localhost/myapp', {
+  useMongoClient: true,
+  /* other options */
+});
+db_promise.then(function(db) {
+});
 
 //routes (eventually can refactor to be more modular)
 app.get('/signup', function (req, res) {
