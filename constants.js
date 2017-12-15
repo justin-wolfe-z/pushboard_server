fcce319dda3055dad22ec632360033944bc73bd8
justin-wolfe-z <justin.wolfe@zapier.com>
2017-12-15 16:33:49 -0500

    some small tweaks to default button setup

--- constants.js
+++ constants.js
@@ -18,63 +18,63 @@ const constants = {
 			id:0,
 			icon:"smiley",
 			type:"static",
-			text:"",
+			text:"Button 0 Text",
 			hookURL:['https://hooks.zapier.com/hooks/catch/2003878/swt611/','https://hooks.zapier.com/hooks/catch/2003878/swt611/','https://hooks.zapier.com/hooks/catch/2003878/swt611/']
 		},
 		{
 			id:1,
 			icon:"baby",
 			type:"static",
-			text:"",
+			text:"Button 1 Text",
 			hookURL:[]
 		},
 		{
 			id:2,
 			icon:"fish",
 			type:"static",
-			text:"",
+			text:"Button 2 Text",
 			hookURL:[]
 		},
 		{
 			id:3,
 			icon:"car",
 			type:"static",
-			text:"",
+			text:"Button 3 Text",
 			hookURL:[]
 		},
 		{
 			id:4,
 			icon:"dog",
 			type:"static",
-			text:"",
+			text:"Button 4 Text",
 			hookURL:[]
 		},
 		{
 			id:5,
 			icon:"cat",
 			type:"static",
-			text:"",
+			text:"Button 5 Text",
 			hookURL:[]
 		},
 		{
 			id:6,
 			icon:"clock",
 			type:"static",
-			text:"",
+			text:"Button 6 Text",
 			hookURL:[]
 		},
 		{
 			id:7,
 			icon:"lunch",
 			type:"static",
-			text:"",
+			text:"Button 7 Text",
 			hookURL:[]
 		},
 		{
 			id:8,
 			icon:"apple",
 			type:"static",
-			text:"",
+			text:"Button 8 Text",
 			hookURL:[]
 		}					
 	]
