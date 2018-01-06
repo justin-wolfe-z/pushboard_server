19e8845beabf1b1cf92a17f6807b37842b9a34ac
justin-wolfe-z <justin.wolfe@zapier.com>
2018-01-06 15:29:39 -0500

    let's try explicitly setting the origin

--- utils.js
+++ utils.js
@@ -135,7 +135,7 @@ const manageHooks = (action, zapier, user) => {
 
 //from https://gist.github.com/cuppster/2344435
 const allowCrossDomainMiddle = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://f00ae30f.ngrok.io');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
     res.header('Access-Control-Max-Age', 1000); 
